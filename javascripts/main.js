9cc40f40dd69e991be5041ea189ae2d6de821850
Alex <alex@willner.ws>
2012-06-27 11:32:16 +0200

    cleanup; content; restructuring

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,13 @@
 console.log('This would be the main JS file.');
+
+
+$(document).ready(function() {
+
+	/* Using custom settings */
+	
+	$("a.screencast").fancybox({
+		'hideOnContentClick': false
+	});
+
+	
+});
